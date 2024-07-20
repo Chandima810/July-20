9e811ca01b40e4ecfe1cd88f1f8ec05bb21cdeb7
Chandima810 <cpg810@gmail.com>
2024-07-20 15:43:03 +0530

    Draft 1 app

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log("Hello Chandima");
+
+const vegitables = [
+    {
+        vegiA: "pumpkins",
+        protien: 20,
+        fat: 10,
+        carbo: 23,
+        fiber: 10, 
+    },
+    
+    {
+        vegiB: "beens", 
+        protien: 20,
+        fat: 10,
+        carbo: 23,
+        fiber: 10, 
+    },
+
+    {
+        vegiB: "cerials", 
+        protien: 20,
+        fat: 10,
+        carbo: 2300,
+        fiber: 10, 
+    },
+]
+   
+
+
+const weight =[
+
+{
+    groupA:60,
+    protien: 100,
+    fat: 50,
+    carb: 200,
+    fiber: 180,
+},
+
+{
+    groupB:80,
+    protien: 120,
+    fat: 60,
+    carb: 250,
+    fiber: 280,
+},
+
+]
+
+console.log (vegitables);
+console.log ([weight]);
